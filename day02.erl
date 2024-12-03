017f8b1b9098b05b1eb31f47dfc5ebd8bd8f5007
Per Hellström <perlarsgustav@gmail.com>
2024-12-03 13:54:00 +0100

    Update day02.erl

--- day02.erl
+++ day02.erl
@@ -3,7 +3,7 @@
 -export([part_one/1, part_two/1]).
 
 part_one(Input) ->
-	Lists = util:read_lines("d2test.txt"),
+	Lists = util:read_lines(Input),
 	SafeLists = lists:map(fun(List) -> util:safe_list(util:convert(List)) end, Li),
 	lists:sum([if L -> 1; true -> 0 end || L <- SafeLists]).
 
